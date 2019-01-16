ba1018d68209c22974fedd13f5c24417b9c4de3a
SgtStarshine <starshine621@ymail.com>
2019-01-15 22:53:39 -0500

    added necessary directories

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Smokehouse', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheese', false);
